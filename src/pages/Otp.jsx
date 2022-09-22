b3777848325d45b3ac77628431dac1cf308fd9d9
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-09-22 04:10:53 +0100

    oh

--- src/pages/Otp.jsx
+++ src/pages/Otp.jsx
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  Flex,
+  HStack,
+  PinInput,
+  PinInputField,
+  Spinner,
+  Text,
+  useToast,
+} from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
 const Otp = () => {
-  return <div></div>;
+  const [pin, setPin] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [correctOtp, setCorrectOtp] = useState("");
+  const [userInfo, setUserInfo] = useState({});
+
+  const toast = useToast();
+  const navigate = useNavigate();
+  return (
+    <>
+      <Flex
+        align-items="center"
+        justify-contents="cener"
+        w={"50%"}
+        m="auto"
+        flexDirection="column"
+      >
+        <Box>
+          <Text
+            color="blue.500"
+            fontSize={50}
+            textAlign="center"
+            fontFamily="sans-serif"
+          >
+            VERIFY OTP
+          </Text>
+          <HStack>
+            <PinInput otp onChange={(otp) => setPin(otp)}>
+              <PinInputField />
+              <PinInputField />
+              <PinInputField />
+              <PinInputField />
+              <PinInputField />
+              <PinInputField />
+            </PinInput>
+          </HStack>
+          <Button
+            marginTop={30}
+            backgroundColor="blue.500"
+            color="white"
+            // onClick={handleSubmit}
+            disabled={pin < 6 || loading}
+          >
+            {loading ? (
+              <Spinner
+                thickness="4px"
+                speed="0.65s"
+                emptyColor="gray.200"
+                color="blue.500"
+                size="md"
+              />
+            ) : (
+              "Submit"
+            )}
+          </Button>
+        </Box>
+      </Flex>
+    </>
+  );
 };
 
 export default Otp;
