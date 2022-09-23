7e1939f233a4b96ebf54a066825235724822305e
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-09-23 15:33:37 +0100

    not working on the profile right now,might work on it later

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import SignUp from "./pages/SignUp";
 import DashBoard from "./pages/DashBoard";
-import Profile from "./pages/Profile";
 import Login from "./pages/LogIn";
 import Otp from "./pages/Otp";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -13,7 +12,6 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/otp" element={<Otp />} />
           <Route path="/dashboard" element={<DashBoard />} />
-          <Route path="/profile" element={<Profile />} />
         </Routes>
       </Router>
     </>
